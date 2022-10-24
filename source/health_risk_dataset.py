18427a0aa5b558bbfea10746a1a6f40d5de9dabb
Denys Herasymuk <herasymuk@ucu.edu.ua>
2022-10-25 01:52:58 +0300

    Added health risk dataset results

---
+++ source/health_risk_dataset.py
@@ -0,0 +1,28 @@
+from river import stream
+from river.datasets import base
+
+
+class HealthRiskDataset(base.FileDataset):
+    def __init__(self, directory="../datasets", filename="Maternal-Health-Risk-Data-Set.csv", delimiter=','):
+        super().__init__(
+            filename=filename,
+            directory=directory,
+            n_features=6,
+            task=base.BINARY_CLF,
+        )
+        self.delimiter = delimiter
+
+    def __iter__(self):
+        return stream.iter_csv(
+            self.path,
+            target="RiskLevel",
+            delimiter=self.delimiter,
+            converters={
+                "Age": int,
+                "SystolicBP": int,
+                "DiastolicBP": int,
+                "BS": float,
+                "BodyTemp": float,
+                "HeartRate": int,
+            },
+        )
