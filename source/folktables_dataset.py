a12366cf9290b808df78c605af2e245de0e76693
Denys Herasymuk <herasymuk@ucu.edu.ua>
2022-10-25 18:04:13 +0300

    Added EDA for folktables

---
+++ source/folktables_dataset.py
@@ -0,0 +1,39 @@
+from river import stream
+from river.datasets import base
+
+
+class FolktablesDataset(base.FileDataset):
+    def __init__(self, directory="../datasets", filename="folktables-NY-2018.csv", delimiter=','):
+        super().__init__(
+            filename=filename,
+            directory=directory,
+            n_features=16,
+            task=base.BINARY_CLF,
+        )
+        self.delimiter = delimiter
+
+    def __iter__(self):
+        return stream.iter_csv(
+            self.path,
+            target="ESR",
+            delimiter=self.delimiter,
+            converters={
+                "AGEP": int,
+                "SCHL": str,
+                "MAR": str,
+                "RELP": str,
+                "DIS": str,
+                "ESP": str,
+                "CIT": str,
+                "MIG": str,
+                "MIL": str,
+                "ANC": str,
+                "NATIVITY": str,
+                "DEAR": str,
+                "DEYE": str,
+                "DREM": str,
+                "SEX": str,
+                "RAC1P": str,
+                "ESR": int,
+            },
+        )
