813b06a6eb015172041249ae2e54b268335f23b8
Denys Herasymuk <herasymuk@ucu.edu.ua>
2022-11-01 00:58:42 +0200

    Added Kappa metric

--- source/river_utils.py
+++ source/river_utils.py
@@ -15,6 +15,7 @@ def ddict2dict(d):
 def evaluate_binary_model(dataset, model, measure_every=1000, dataset_limit=None):
     cm = metrics.ConfusionMatrix()
     acc_metric = metrics.Accuracy()
+    kappa_metric = metrics.CohenKappa()
     # Weighted-average F1 score.
     # This works by computing the F1 score per class,
     # and then performs a global weighted average by using the support of each class.
@@ -22,6 +23,7 @@ def evaluate_binary_model(dataset, model, measure_every=1000, dataset_limit=None
 
     acc_metrics = []
     f1_metrics = []
+    kappa_metrics = []
     for idx, (x, y_true) in enumerate(dataset):
         # Obtain the prior prediction and update the model in one go
         y_pred = model.predict_one(x)
@@ -30,11 +32,13 @@ def evaluate_binary_model(dataset, model, measure_every=1000, dataset_limit=None
         if y_pred is not None:
             cm = cm.update(y_true, y_pred)
             acc_metric = acc_metric.update(y_true, y_pred)
+            kappa_metric = kappa_metric.update(y_true, y_pred)
             f1_metric = f1_metric.update(y_true, y_pred)
         if (idx + 1) % measure_every == 0:
             acc_metrics.append(acc_metric.get())
+            kappa_metrics.append(kappa_metric.get())
             f1_metrics.append(f1_metric.get())
-            print(f'Index: {idx + 1}; {acc_metric}; {f1_metric}')
+            print(f'Index: {idx + 1}; {acc_metric}; {kappa_metric}; {f1_metric}')
 
         # Shrink the dataset if needed
         if dataset_limit is not None and (idx + 1) == dataset_limit:
@@ -51,6 +55,12 @@ def evaluate_binary_model(dataset, model, measure_every=1000, dataset_limit=None
     ax.grid(alpha=0.75)
     ax.plot(acc_metrics, lw=3, color='#2ecc71', alpha=0.8, label='Accuracy')
     ax.set_title(acc_metric)
+
+    # Plot the Accuracy results
+    fig, ax = plt.subplots(figsize=(10, 6))
+    ax.grid(alpha=0.75)
+    ax.plot(kappa_metrics, lw=3, color='blue', alpha=0.8, label='CohenKappa')
+    ax.set_title(kappa_metric)
 
     # Plot the F1 results
     fig, ax = plt.subplots(figsize=(10, 6))
